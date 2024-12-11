2add4e9779f8e193b6f23a9c845ad713df37807c
Elisabethbanjarnahor <elisabethmarbun34@gmail.com>
2024-12-11 11:33:22 +0700

    Delete input_form.py

--- input_form.py
+++
@@ -1,7 +0,0 @@
-class InputForm:
-    @staticmethod
-    def input_data():
-        nim = input("Masukkan NIM: ")
-        nama = input("Masukkan Nama: ")
-        prodi = input("Masukkan Prodi: ")
-        return nim, nama, prodi
